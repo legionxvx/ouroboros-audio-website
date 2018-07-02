2a37ecd2c4f5f4d0ea02fe57d2cc287ce40d5379
Nikolaus Gullotta <nikolaus.gullotta@gmail.com>
2018-07-02 13:31:37 -0500

    moved and reanmed flaskwriter project to it's own repo because it was outgrowing this one.

--- flask/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#use LOCATION=local/remote depending on what your implementation is
-#this affects the CSS imports, in our case, we needed it to be changed
-#to url_for(/flask/) as root
-rm flask-writer.pyc
-export LOCATION=local
-export FLASK_ENV=development
-export FLASK_APP=flask-writer.py
-python -m flask run
